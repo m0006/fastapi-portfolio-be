4c87db3d5e38d77ee4e1fff5e2e717cb456caeaa
m0006 <26069943+m0006@users.noreply.github.com>
2024-06-07 23:18:54 -0400

    editing docs desc

--- main.py
+++ main.py
@@ -11,7 +11,13 @@ API_PREFIX = "/api/v1"
 
 DOCS_DESCRIPTION = """
     ## Notes:
-    - Dataset used and modified for taxi_movement API: [Kaggle: Rome Taxi Data (subset)](https://www.kaggle.com/datasets/asjad99/rome-taxi-data-subset).\
+    - Datasets used and modified for API apps:
+        - __housing_app__ API:
+            - [Kaggle: USA Housing Listings](https://www.kaggle.com/datasets/austinreese/usa-housing-listings)
+            - [MassGIS Data: MBTA Rapid Transit (December 2022 edition)](https://www.mass.gov/info-details/massgis-data-mbta-rapid-transit)
+            - [MassGIS Data: Trains (November 2023 edition)](https://www.mass.gov/info-details/massgis-data-trains)
+        - __taxi_movement__ API:
+            - [Kaggle: Rome Taxi Data (subset)](https://www.kaggle.com/datasets/asjad99/rome-taxi-data-subset)
 """
 
 
